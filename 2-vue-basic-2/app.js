095a79579b793b3ba1c16cde4e5662c688a181ff
DongNV <chomottinhyeu127@gmail.com>
2021-10-22 16:41:11 +0700

    ending with lession 30 31

--- 2-vue-basic-2/app.js
+++ 2-vue-basic-2/app.js
@@ -3,24 +3,68 @@ const app = Vue.createApp({
         return {
             counter : 10,
             name: '',
-            confirmName: ''
+            lastName: '',
+            // fullname: ''
         };
     },
+    watch: {
+        counter(value) {
+            if (value > 50) {
+                const that = this
+                setTimeout(function() {
+                    that.counter = 0
+                }, 2000)
+            }
+        }
+        // name (value) {
+        //     if (value === '') {
+        //         this.fullname = ''
+        //     } else {
+        //         this.fullname = this.name + ' ' + this.lastName
+        //     }
+        // },
+        // lastName (value) {
+        //     if (value === '') {
+        //         this.fullname = ''
+        //     } else {
+        //         this.fullname = this.name + ' ' + this.lastName
+        //     }
+        // }
+    },
+    computed: {
+        fullname() {
+            // console.log('Running again...')
+            if(this.name === ''|| this.lastName === '') {
+                return ''
+            }
+            return this.name + ' ' + this.lastName;
+        }
+    },
     methods: {
-        confirmInput() {
-            this.confirmName = this.name
-        },
+        // confirmInput() {
+        //     this.confirmedName = this.name
+        // },
+        // outputFullname() {
+        //     if(this.name === '') {
+        //         return ''
+        //     }
+        //     return this.name + ' Nguyen Van';
+        // },
         submitForm() {
             alert('Submitted!')
         },
-        setName(event,lastName) {
-            this.name = event.target.value + ' ' + lastName
+        setName(event) {
+            this.name = event.target.value
         },
         add(num) {
             this.counter = this.counter + num;
         },
         reduce(num) {
             this.counter = this.counter - num;
+        },
+        resetInput() {
+            this.name = '';
+            this.lastName = '';
         }
     }
 })
