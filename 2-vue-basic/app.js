2a6e6d7249dfc7e94279cfd21af90af511d6f843
DongNV <chomottinhyeu127@gmail.com>
2021-10-22 13:23:00 +0700

    add ex 2 vue-basic

---
+++ 2-vue-basic/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data () {
+        return {
+            courseGoal: 'Finish the course anh learn Vue!',
+            vueLink: 'https://toidicodedao.com/'
+        }
+    }
+});
+
+app.mount('#user-goal');
